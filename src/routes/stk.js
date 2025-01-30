4fa9f6f3825af34788878e8a4e2a06271afe1ebb
Jordant007 <jordantyrill@gmail.com>
2025-01-30 14:51:21 +0300

    created stk route

--- src/routes/stk.js
+++ src/routes/stk.js
@@ -2,7 +2,7 @@ const express = require("express");
 const {Router} = express
 const router =router()
 
-router.get("/stk",(req,res)=>{
+router.get("/stk", async (req,res)=>{
     try {
         const token = await mpesaAccessToken()
         const url ="https://sandbox.safaricom.co.ke/oauth/v1/generate?grant_type=client_credentials"
@@ -12,18 +12,25 @@ router.get("/stk",(req,res)=>{
             "Timestamp":"20160216165627",    
             "TransactionType": "CustomerPayBillOnline",    
             "Amount": "1",    
-            "PartyA":"254708374149",    
+            "PartyA":"254710148740",    
             "PartyB":"174379",    
-            "PhoneNumber":"254708374149",    
-            "CallBackURL": "https://mydomain.com/pat",    
+            "PhoneNumber":"254710148740",    
+            "CallBackURL": "https://connectease-backend-eeda.onrender.com/api/mpesa/stk",    
             "AccountReference":"Test",    
             "TransactionDesc":"Test"
          }
-         const optionsadd  ={
+         const options  ={
             method:"POST",
             headers:{"content-Type":"application/json",Authorization:`Bearer ${token.access_token}`},
             body: JSON.stringify(body)
-         }
+         };
+         const response = fetch (url,options)
+         const data = await(response).json()
+
+         console.log(req.body)
+         return res.status(200).json(req.body)
+
+         
     } catch (error) {
        res.status(500).json({message:error})
     }
